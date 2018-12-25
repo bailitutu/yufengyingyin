5a127afda3142fcf898c49c4d954055473bb4025
bailitutu <1104087243@qq.com>
2018-12-25 20:09:34 +0800

    首页部分界面

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -6,7 +6,26 @@
                 <button class="more_btn fr">MORE>></button>
             </div>
         </div>
+        <div class="sec bg-white">
+            <div class="content">
 
+
+            </div>
+        </div>
+
+        <div class="sec contact_sec">
+            <div class="content contact_content">
+                <div class="map_item">
+
+                </div>
+                <div class="info_item">
+                    <h3>湖南驭风者影视文化传媒有限公司</h3>
+                    <p>联系电话：400-0980-0880</p>
+                    <p>企业邮箱：400-0980-0880</p>
+                    <p>公司地址：湖南省长沙市阜宁县红岭路498号</p>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -43,6 +62,35 @@
             .more_btn{
                 padding:0 10px;
                 background: @c-white;
+            }
+        }
+    }
+    .contact_sec{
+        background: @bg-black;
+        .contact_content{
+            height:450px;
+            padding-top:35px;
+            .map_item{
+                width:624px;
+                height:380px;
+                background: red;
+                float: left;
+            }
+            .info_item{
+                float: left;
+                width:500px;
+                height:380px;
+                margin-left: 43px;
+                color: @c-white;
+                h3{
+                    font-size:30px;
+                    margin-top: 72px;
+                    margin-bottom:28px;
+                }
+                p{
+                    font-size:18px;
+                    padding-top:24px;
+                }
             }
         }
     }
